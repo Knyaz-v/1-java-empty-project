3034d396a7ce24b2e00c923dad41aa14dc28b418
Knyaz-v <animechnik02@gmail.com>
2026-03-02 01:30:16 +0500

    Применение патча подключения базы данных

--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -1,29 +1,25 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Post;
+import com.example.demo.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.StreamSupport;
 
 @Service
 public class PostService {
-    private List<Post> posts;
-
-    {
-        posts = new ArrayList<>();
-        posts.add(new Post(0L,"Недавно вышла новенькая Reanimal", new Date()));
-        posts.add(new Post(1L,"Ждем выхода кооперативной Burglin' Gnomes", new Date()));
-        posts.add(new Post(2L,"Сколько еще ждать TES6???", new Date()));
-    }
+    @Autowired
+    PostRepository postRepository;
 
     public List<Post> listAllPosts() {
-        return posts;
+        return StreamSupport.stream(postRepository.findAll().spliterator(), false).toList();
     }
 
     public void create(String text) {
-        Long newId = (long)posts.size();
-        posts.add(new Post(newId, text, new Date()));
+        Post post = new Post(null, text, new Date());
+        postRepository.save(post);
     }
 }
